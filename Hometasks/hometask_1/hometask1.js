c098d60a767bf7a124e4ccee9727da2a6b59892e
Shabazzt <mrsherlock@mail.ru>
2021-10-27 18:53:00 +0300

    half

--- Hometasks/hometask_1/hometask1.js
+++ Hometasks/hometask_1/hometask1.js
@@ -1,3 +1,14 @@
  let firstName = prompt("Введите ваше имя");
  let secondName = prompt("Введите вашу фамилию");
  let thirdName = prompt("Введите ваше отчество");
+ let age = parseInt(prompt("Укажите ваш возраст"));
+ let gender = confirm("Укажите ваш пол");
+ let ageTranslatedInDays = age * 365;
+ let ageAfterFiveYears = age + 5;
+
+ alert("Ваше Ф.И.О:" + " " + firstName + " " + secondName + " " + thirdName + "\n" +
+     "Ваш возраст в годах:" + " " + age + "\n" +
+     "Ваш возраст в днях:" + " " + ageTranslatedInDays + "\n" +
+     "Через 5 лет вам будет:" + " " + AfterFiveYears + "\n" +
+     "Ваш пол:"
+ );
